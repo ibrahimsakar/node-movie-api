e05b8f4934bd27e806911a6748b0982153015f55
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-08 10:12:50 +0300

    Model folder and Movie model added.

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const index = require('./routes/index');
+const movie = require('./routes/movie');
 
 const app = express();
+
+//db connection
+const db = require('./helper/db.js')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/api/movie', movie);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) =>{
---
+++ helper/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://ibrahim_movie:123456@ds115110.mlab.com:15110/movie-api');
+
+    mongoose.connection.on('open', () => {
+        console.log("MongoDB connected.");
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log("MongoDB error.", err);
+    });
+};
