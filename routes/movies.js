229598a72234c12f6d9992df7100afe7ec81c795
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-10 13:49:36 +0300

    /api/movies/:movie_id get endpoint added.

--- routes/movies.js
+++ routes/movies.js
@@ -4,14 +4,26 @@ const router = express.Router();
 //Models
 const Movie = require('../models/Movie');
 
+//Routing
 router.get('/', (req, res) => {
-  const promise = Movie.find({});
+  const promise = Movie.find({ });
   promise.then((data) => {
     res.json(data);
   }).catch((err) =>{
     res.json(err);
   })
 });
+
+router.get('/:movie_id', (req, res) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((movie) => {
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
 router.post('/', (req, res, next) => {
   const { title, category, country, year, imdb_score } = req.body;
   
