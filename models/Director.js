5c0ae100860294b8c85161492093e07525445ef4
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-11 11:40:54 +0300

    Validations for Movie and Director models.

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        minlength: 2,
+        maxlength: 60
+    },
+    surname: {
+        type: String,
+        minlength: 2,
+        maxlength: 60
+    },
+    bio: {
+        type: String,
+        minlength: 10,
+        maxlength: 1000
+    },
     createdAt: {
         type: Date,
         default: Date.now
--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,29 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '{PATH} is cannot be empty'],
+        maxlength: 100
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: { 
+        type: String,
+        maxlength: 15,
+        minlength: 3
+    },
+    country: {
+        type: String,
+        minlength: 1,
+        maxlength: 50
+    },
+    year: {
+        type: Number,
+        min: 1900,
+        max: 2030
+    },
+    imdb_score: {
+        type: Number,
+        min: 0,
+        max: 10
+    },
     createdAt: {
         type: Date,
         default: Date.now
