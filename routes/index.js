d6f66f366b6b99ab3c01567a17eea1f91aad13ea
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-11 19:27:58 +0300

    Bcrypt package added.

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,20 @@ router.get('/', (req, res, next) => {
 router.post('/register', (req, res, next) => {
   const { username, password } = req.body;
 
-  const user = new User({ username, password });
-
-  const promise = user.save();
-  promise.then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.json(err);
-  })
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({ 
+      username,
+      password: hash
+    });
+    
+    const promise = user.save();
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    })
+  });
 });
+ 
 
 module.exports = router;
