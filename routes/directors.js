63ae8691a44523e9f2edf0a756a71d076d4161fb
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-11 10:59:31 +0300

    /api/directors [GET] method added.

--- routes/directors.js
+++ routes/directors.js
@@ -4,7 +4,54 @@ const router = express.Router();
 //Models
 const Director = require('../models/Director');
 
-/* GET home page. */
+//Routing 
+
+router.get('/', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ]);
+  
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) =>{
+    res.json(err);
+  })
+});
+
 router.post('/', (req, res, next) => {
   const director = new Director(req.body);
   const promise = director.save();
