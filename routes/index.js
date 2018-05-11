c75e4cac84b96ca081d15e0e880126ebf28c3a66
İbrahim Şakar <ibrahimsakar@Ibrahim-MacBook-Pro.local>
2018-05-11 23:25:03 +0300

    JSON Web Token verify.

--- routes/index.js
+++ routes/index.js
@@ -13,56 +13,61 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/register', (req, res, next) => {
-  const { username, password } = req.body;
+	const { username, password } = req.body;
 
-  bcrypt.hash(password, 10).then((hash) => {
-    const user = new User({ 
-      username,
-      password: hash
-    });
+	bcrypt.hash(password, 10).then((hash) => {
+		const user = new User({
+			username,
+			password: hash
+		});
 
-    const promise = user.save();
-    promise.then((data) => {
-      res.json(data);
-    }).catch((err) => {
-      res.json(err);
-    })
-  });
+		const promise = user.save();
+		promise.then((data) => {
+			res.json(data)
+		}).catch((err) => {
+			res.json(err);
+		})
+	});
 });
 
 router.post('/authenticate', (req, res) => {
-  const { username, password } = req.body;
-  
-  User.findOne({
-    username
-  }, (err, user) => {
-    if (err)
-      throw err;
-    if (!user){
-      res.json({
-        status: false,
-        message: 'Authentication failed, user not found!'
-      });
-    } else {
-      bcrypt.compare(password, user.password).then((result) => {
-        if(!result){
-          res.json({ 
-            status: false, 
-            message: 'Authentication failed, wrong password!' 
-          });
-        } else{
-          const payload = { username };
-          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
-            expiresIn: 720 // 12 hours 
-          });
-          res.json({ 
-            status: true, 
-            token 
-          }); 
-        }
-      })
-    }
-  });
+	const { username, password } = req.body;
+
+	User.findOne({
+		username
+	}, (err, user) => {
+		if (err)
+			throw err;
+
+		if(!user){
+			res.json({
+				status: false,
+				message: 'Authentication failed, user not found.'
+			});
+		}else{
+			bcrypt.compare(password, user.password).then((result) => {
+				if (!result){
+					res.json({
+						status: false,
+						message: 'Authentication failed, wrong password.'
+					});
+				}else{
+					const payload = {
+						username
+					};
+					const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+						expiresIn: 720 // 12 saat
+					});
+
+					res.json({
+						status: true,
+						token
+					})
+				}
+			});
+		}
+	});
+
 });
  
 
